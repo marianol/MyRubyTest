81070f8bc4ea132fa9555fb6ecf60cbe892b2735
Mariano Luna <mariano@etszone.com>
2010-12-09 15:53:14 -0600

    Added user basic validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101209193533) do
+ActiveRecord::Schema.define(:version => 20101209214822) do
 
   create_table "customers", :force => true do |t|
     t.string   "name"
@@ -26,5 +26,7 @@ ActiveRecord::Schema.define(:version => 20101209193533) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
