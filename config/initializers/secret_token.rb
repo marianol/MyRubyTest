b7d06647a526a3a82dc344e24568d12da33f84c1
Mariano Luna <mariano@etszone.com>
2010-12-09 11:44:34 -0600

    Fist Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CustomerManager::Application.config.secret_token = '1e864669237abc25bca151e2f18018b67670e2a93aa2d485f63bd1f39e5389e2b3f2cdf467bffa39cf0b1bf82f24bed3d53cc1a0620aa474a943d8f5213579ba'
