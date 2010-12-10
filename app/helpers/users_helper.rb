839e3d7ae62200c62f33161fb899c01a42df1c95
Mariano Luna <mariano@etszone.com>
2010-12-09 18:18:43 -0600

    Styled user views added gravatar gem

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+  
+  def gravatar_for(user, options = { :size => 50 })
+    gravatar_image_tag(user.email.downcase, :alt => user.name,
+                                            :class => 'gravatar',
+                                            :gravatar => options)
+  end
+  
+  def active_icon_for(user)
+    user.active ? image_tag('tick.png') : image_tag('cross.png') 
+  end
 end
